1052757bfae3648eda6a16ab65eef7085cd2fc1e
Dmitrii-Petrov <BanananaBatman@yandex.ru>
2023-07-08 20:17:21 +0300

    ТЗ-16 первая итерация

--- gateway/src/main/java/ru/practicum/shareit/exceptions/UnknownStateException.java
+++ gateway/src/main/java/ru/practicum/shareit/exceptions/UnknownStateException.java
@@ -1,5 +1,9 @@
 package ru.practicum.shareit.exceptions;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.BAD_REQUEST)
 public class UnknownStateException extends RuntimeException {
     public UnknownStateException(String s) {
         super(s);
