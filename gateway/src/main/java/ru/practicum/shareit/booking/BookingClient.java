4765e30e3365dfd0098d9f1969f16eb21315fe01
Dmitrii-Petrov <BanananaBatman@yandex.ru>
2023-07-08 18:42:20 +0300

    ТЗ-16 первая итерация

--- gateway/src/main/java/ru/practicum/shareit/booking/BookingClient.java
+++ gateway/src/main/java/ru/practicum/shareit/booking/BookingClient.java
@@ -8,7 +8,6 @@ import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.web.util.DefaultUriBuilderFactory;
 import ru.practicum.shareit.booking.dto.BookItemRequestDto;
-import ru.practicum.shareit.booking.dto.BookingState;
 import ru.practicum.shareit.client.BaseClient;
 
 import java.util.Map;
